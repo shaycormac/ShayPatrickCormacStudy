3d11ef8533b1ab9b670f6302cb4c2932cb7ae7f0
shaycormac <574583006@qq.com>
2017-05-20 16:11:26 +0800

    动画学习

---
+++ animationstudy/src/main/java/com/assassin/anim/MainActivity.java
@@ -0,0 +1,111 @@
+package com.assassin.anim;
+
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.assassin.anim.activity.AnimaButtonActivity;
+import com.assassin.anim.widget.ViewWrapper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnAnim;
+    private FrameLayout flAnimation;
+    //控件的高度
+    private int flWidth;
+    //
+    private boolean isFirst;
+    //
+    private boolean isCalculat=true;
+
+    private Button btnAnimaTest;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAnim = (Button) findViewById(R.id.btnAnim);
+        btnAnimaTest = (Button) findViewById(R.id.btnAnimaTest);
+        //使用View动画
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.shay_anim_view);
+       // btnAnim.startAnimation(animation);
+        //使用属性动画
+        flAnimation = (FrameLayout) findViewById(R.id.flAnimation);
+        //在这里获取不到控件的宽高，只有onCreate方法执行完了之后才可以！！
+        Log.d("宽幅", flAnimation.getHeight() +"");
+       // ObjectAnimator.ofInt(flAnimation,"height",200).setDuration(5000).start();
+
+        ViewTreeObserver observer = flAnimation.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() 
+            {
+                //绘制好了。，获取当前的宽高
+                if (isCalculat)
+                {
+                    flWidth = flAnimation.getHeight();
+                    isCalculat = false;
+                }
+                
+            }
+        });
+        //属性动画改变背景
+        ValueAnimator colorAnim = ObjectAnimator.ofInt(flAnimation, "backgroundColor", 0Xffff8080, 0xff8080ff);
+        colorAnim.setDuration(3000);
+        colorAnim.setEvaluator(new ArgbEvaluator());
+        colorAnim.setRepeatCount(ValueAnimator.INFINITE);
+        colorAnim.setRepeatMode(ValueAnimator.REVERSE);
+        colorAnim.start();
+
+        AnimatorSet animator = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.propertity_animator_stydy);
+        animator.setTarget(btnAnim);
+        animator.start();
+        
+        //通过包装类，改变控件的属性
+        final ViewWrapper wrapper = new ViewWrapper(flAnimation);
+      //  ObjectAnimator.ofInt(wrapper,"height",800,100).setDuration(5000).start();
+
+        btnAnim.setOnClickListener(new View.OnClickListener() 
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (isFirst)
+                {
+                    //展开
+                    isFirst = false;
+                    ObjectAnimator.ofInt(wrapper,"height",0,flWidth).setDuration(1000).start();
+                }else 
+                {
+                    isFirst = true;
+                    //闭合
+                    ObjectAnimator.ofInt(wrapper,"height",flWidth,0).setDuration(1000).start();
+                }
+                
+                
+            }
+        });
+        
+        
+        btnAnimaTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AnimaButtonActivity.class));
+            }
+        });
+    }
+
+
+    
+}
--- app/src/main/java/com/assassin/shaystudy/widget/SpringScrollView.java
+++ app/src/main/java/com/assassin/shaystudy/widget/SpringScrollView.java
@@ -19,6 +19,7 @@ import android.view.MotionEvent;
 /**
  * Created by SouthernBox on 2017/3/23 0023.
  * 下拉后有回弹动画的滚动控件
+ * 不足点：上拉时候会覆盖上面的控件，需要再套上一层FrameLayout才能解决这个问题。
  */
 
 public class SpringScrollView extends NestedScrollView {
@@ -40,14 +41,15 @@ public class SpringScrollView extends NestedScrollView {
         //刚度 默认1200 值越大回弹的速度越快
         springAnim.getSpring().setStiffness(800.0f);
         //阻尼 默认0.5 值越小，回弹之后来回的次数越多
-        springAnim.getSpring().setDampingRatio(0.50f);
+        springAnim.getSpring().setDampingRatio(0.4f);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent e) {
         switch (e.getAction()) {
             case MotionEvent.ACTION_MOVE:
-                if (getScrollY() <= 0) {
+                if (getScrollY() <= 0) 
+                {
                     //顶部下拉
                     if (startDragY == 0) {
                         startDragY = e.getRawY();
@@ -60,7 +62,8 @@ public class SpringScrollView extends NestedScrollView {
                         springAnim.cancel();
                         setTranslationY(0);
                     }
-                } else if ((getScrollY() + getHeight()) >= getChildAt(0).getMeasuredHeight()) {
+                } else if ((getScrollY() + getHeight()) >= getChildAt(0).getMeasuredHeight()) 
+                {
                     //底部上拉
                     if (startDragY == 0) {
                         startDragY = e.getRawY();
