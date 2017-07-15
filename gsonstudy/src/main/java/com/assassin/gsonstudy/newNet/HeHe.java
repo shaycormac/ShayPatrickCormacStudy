4f4c685c45b079bc968a01c3bba4d60ffde4c856
ShayPatrickCormac <574583006@qq.com>
2017-07-15 12:46:25 +0800

    添加自定义的ImageGetter，测试

---
+++ gsonstudy/src/main/java/com/assassin/gsonstudy/newNet/HeHe.java
@@ -0,0 +1,12 @@
+package com.assassin.gsonstudy.newNet;
+
+/**
+ * @author 作者：Shay-Patrick-Cormac
+ * @datetime 创建时间：2017/7/15 12:45
+ * @email 邮箱： 574583006@qq.com
+ * @content 说明：创建这个类的意义，目的。
+ */
+public class HeHe 
+{
+    public static final String dd ="www.shaycormaFang4788.com.cn";
+}
